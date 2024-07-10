11f3f7420afc8d097d8b214dae56657496303f0b
Simon Sprouse <simonhsprouse@gmail.com>
2024-07-10 18:34:14 -0500

    new array mechanism

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import TextEditor from './TextEditor';
+import ImageUploader from './ImageUploader';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Bungus</p>
+        <TextEditor />
+        <ImageUploader />
       </header>
     </div>
   );
